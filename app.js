0db18cd268df9cfbb4e72e38950183ad023977d4
Derek Lee <derekleehh@ncs.com.sg>
2018-04-16 14:06:16 +0800

    added all nessary codes for api endpoint

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var users = require('./routes/users');
 var app = express();
 var bluebird = require('bluebird')
 
+
+var api = require('./routes/api.route')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -35,8 +38,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Use the Routes
 app.use('/', index);
 app.use('/users', users);
+
+//Use the API routes for all routes matching /api
+
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
